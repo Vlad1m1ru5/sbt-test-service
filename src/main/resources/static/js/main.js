40bcb460fdcebfbd6c6389f74a7ed27b6d75d9aa
Vlad1m1ru5 <kubantsev.vov@mail.ru>
2019-05-11 18:13:18 +0300

    "Начало настройки js."

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,7 +1,22 @@
 
+Vue.component('messages-row', {
+    props: ['message'],
+    template: '<div><i>({{message.id}})</i> {{message.text}}</div>'
+});
+
+Vue.component('messages-list', {
+    props:  ['messages'],
+    template: '<div><messages-row v-for="message in messages" :message="message"/></div>'
+});
+
 var app = new Vue({
     el: '#app',
+    template: '<messages-list :messages="messages"/>',
     data: {
-        message: 'Здесь будет контент!'
+        messages: [
+            {id: '123', text: "Что-то"},
+            {id: '3', text: "Здесь"},
+            {id: '12', text: "Написано"}
+        ]
     }
 });
